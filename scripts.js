370fe573a9b3da1a17e9063018f70ee6998d0f6a
Dominik Croci <dominikrcroci@gmail.com>
2023-04-04 21:12:07 -0700

    function is best of 5 game

--- scripts.js
+++ scripts.js
@@ -5,36 +5,42 @@ let playerChoice;
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * choices.length);
   computerChoice = choices[randomChoice].toLowerCase();
-  console.log(`The computer chose ${computerChoice}`)
+  console.log(`The computer chose ${computerChoice}`);
   return computerChoice;
 }
 function playerSelection() {
-    let playerChoicePrompt = prompt("Rock, Paper, or Scissors").toLowerCase();
-    return playerChoicePrompt;
-  }
+  let playerChoicePrompt = prompt("Rock, Paper, or Scissors").toLowerCase();
+  return playerChoicePrompt;
+}
 function playGame(computer, player) {
   if (player === "rock") {
-    if (computer === 'scissors') {
-        console.log("You won! Rock beats scissors!");
-    }   else {
-        console.log("You lost! Rock loses to paper!");  
+    if (computer === "scissors") {
+      console.log("You won! Rock beats scissors!");
+    } else if (player === computer) {
+      console.log("You tied!");
+    } else {
+      console.log("You lost! Rock loses to paper!");
+    }
   }
-}
   if (player === "paper") {
-    if (computer === 'rock') {
-        console.log("You won! Paper beats rock!");
-    }   else {
-        console.log("You lost! Paper loses to scissors!");  
+    if (computer === "rock") {
+      console.log("You won! Paper beats rock!");
+    } else if (player === computer) {
+      console.log("You tied!");
+    } else {
+      console.log("You lost! Paper loses to scissors!");
+    }
   }
-}
   if (player === "scissors") {
-    if (computer === 'paper') {
-        console.log("You won! Scissors beats paper!");
-    }   else {
-        console.log("You lost! Scissors loses to rock!");  
+    if (computer === "paper") {
+      console.log("You won! Scissors beats paper!");
+    } else if (player === computer) {
+      console.log("You tied!");
+    } else {
+      console.log("You lost! Scissors loses to rock!");
+    }
   }
 }
-  }
-  
-
-playGame(getComputerChoice(), playerSelection());
+for (let i = 0; i < 5; i++) {
+    playGame(getComputerChoice(), playerSelection());
+}
