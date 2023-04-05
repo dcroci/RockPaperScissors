2dfb74b53f9e3168fe3385c358d0519817198823
Dominik Croci <dominikrcroci@gmail.com>
2023-04-04 17:47:29 -0700

    output returns to console

--- scripts.js
+++ scripts.js
@@ -4,38 +4,32 @@ let choices = [
     'scissors'
 ]
 
-function getComputerChoice(){
-    // need to get a random choice between rock, paper, scissors
-    let randomChoice = Math.floor(Math.random() * choices.length);
-    let computerChoice = (choices[randomChoice].toLowerCase());
-    return computerChoice;
-}
 function getPlayerChoice(){
     let playerChoice = prompt('Rock, Paper, or Scissors?');
     playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
+    let randomChoice = Math.floor(Math.random() * choices.length);
+    let computerChoice = (choices[randomChoice].toLowerCase());
+    if(playerChoice === 'rock'){
+        if(computerChoice === 'scissors'){
+            console.log('You win!');
+        } else{
+            console.log('You lost!');
+        }
+    }
+    if(playerChoice === 'paper'){
+        if(computerChoice === 'rock'){
+            console.log('You win!');
+        } else{
+            console.log('You lost!');
+        }
+    }
+    if(playerChoice === 'scissors'){
+        if(computerChoice === 'paper'){
+            console.log('You win!');
+        } else{
+            console.log('You lost!');
+        }
+    }
+    getPlayerChoice();
     // need to prompt user to choose rock, paper, scissors
 }
-
-if(playerChoice === 'rock'){
-    if(computerChoice === 'scissors'){
-        console.log('You win!');
-    } else{
-        console.log('You lost!');
-    }
-}
-if(playerChoice === 'paper'){
-    if(computerChoice === 'rock'){
-        console.log('You win!');
-    } else{
-        console.log('You lost!');
-    }
-}
-if(playerChoice === 'scissors'){
-    if(computerChoice === 'paper'){
-        console.log('You win!');
-    } else{
-        console.log('You lost!');
-    }
-}
-
